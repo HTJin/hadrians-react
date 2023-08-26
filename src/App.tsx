58f68e842eff56614e37f0238996b740de68f448
Hyun-Tae Jin <htjmario@gmail.com>
2023-08-25 23:29:58 -0400

    unused user auth, commented out for now

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,46 @@
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import { SheetLeft } from "./views/SheetLeft";
-import { UserType } from "./types/user";
+// import { UserType } from "./types/user";
 
 function App() {
-  const [isLoading, setIsLoading] = useState(true);
-  const [loggedIn, setLoggedIn] = useState(
-    (localStorage.getItem("token") &&
-      new Date(localStorage.getItem("tokenExp") as string) > new Date()) ||
-      false,
-  );
-  const [loggedInUser, setLoggedInUser] = useState<UserType | null>(null);
+  // const [isLoading, setIsLoading] = useState(true);
+  // const [loggedIn, setLoggedIn] = useState(
+  //   (localStorage.getItem("token") &&
+  //     new Date(localStorage.getItem("tokenExp") as string) > new Date()) ||
+  //     false,
+  // );
+  // const [loggedInUser, setLoggedInUser] = useState<UserType | null>(null);
 
-  useEffect(() => {
-    const getLoggedInuser = async () => {
-      setIsLoading(true);
-      // const token = localStorage.getItem("token");
-      // const response = await getUser(token!);
-      // setLoggedInUser(response.data!)
-    };
-    if (loggedIn) {
-      getLoggedInuser();
-    } else {
-      setIsLoading(false);
-    }
-  }, []);
+  // useEffect(() => {
+  //   const getLoggedInuser = async () => {
+  //     setIsLoading(true);
+  //     // const token = localStorage.getItem("token");
+  //     // const response = await getUser(token!);
+  //     // setLoggedInUser(response.data!)
+  //   };
+  //   if (loggedIn) {
+  //     getLoggedInuser();
+  //   } else {
+  //     setIsLoading(false);
+  //   }
+  // }, []);
 
-  const logUserIn = (user: UserType): void => {
-    setLoggedIn(true);
-    setLoggedInUser(user);
-  };
+  // const logUserIn = (user: UserType): void => {
+  //   setLoggedIn(true);
+  //   setLoggedInUser(user);
+  // };
 
-  const logUserOut = (): void => {
-    setLoggedIn(false);
-    setLoggedInUser(null);
-    localStorage.removeItem("token");
-    localStorage.removeItem("tokenExp");
-  };
+  // const logUserOut = (): void => {
+  //   setLoggedIn(false);
+  //   setLoggedInUser(null);
+  //   localStorage.removeItem("token");
+  //   localStorage.removeItem("tokenExp");
+  // };
 
-  if (isLoading) {
-    return <p>Loading...</p>;
-  }
+  // if (isLoading) {
+  //   return <p>Loading...</p>;
+  // }
 
   return (
     <div className="flex h-screen flex-col items-center bg-slate-900 text-slate-50">
