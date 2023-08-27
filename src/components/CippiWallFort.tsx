0a9aad98d327387e6de3145541c3e3283236e369
Hyun-Tae Jin <htjmario@gmail.com>
2023-08-26 23:23:33 -0400

    corrected arrow specialcase, added grid template

--- src/components/CippiWallFort.tsx
+++ src/components/CippiWallFort.tsx
@@ -134,7 +134,11 @@ export const CippiWallFort = () => {
           <Arrow
             width={index < fortWidths.length ? fortWidths[index] : 1}
             specialCase={
-              index === 3 || index === 10 || index === 17 || index === 20
+              index === 3 ||
+              index === 10 ||
+              index === 13 ||
+              index === 17 ||
+              index === 20
             }
           />
           {renderFortScribbleBox(icon, index)}
